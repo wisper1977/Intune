8a041d19d1d7df48b1a730887769379094d6c29d
Chris Collins <60556503+wisper1977@users.noreply.github.com>
2024-01-24 10:13:03 -0700

    Update and rename Remediation-Steam.ps1 to Steam-Folder-Remediation.ps1

--- Remedations/Steam-Folder-Remediation.ps1
+++ Remedations/Steam-Folder-Remediation.ps1
@@ -1,20 +1,3 @@
-<#
-.Synopsis
-Script to create file and/or folders
-
-.DESCRIPTION
-This script can be used to create one or multiple file and/or folders. It will check if the file and/or folder already exists before creating it.
-
-.NOTES   
-Name: Create-FileAndFolder.ps1
-Created By: Peter Dodemont
-Version: 1.1
-DateUpdated: 12/03/2022
-
-.LINK
-https://peterdodemont.com/
-#>
-
 # Set Variables
 $FolderPaths = @("$Env:userprofile\appdata\Local\steam")
 
